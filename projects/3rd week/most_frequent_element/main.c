7d3fa115e7e965b3bc33595613b4aaaf5566665d
dorksan <alinchasaur@yandex.ru>
2021-10-09 17:17:19 +0300

    Remote parentheses in test function

--- projects/3rd week/most_frequent_element/main.c
+++ projects/3rd week/most_frequent_element/main.c
@@ -89,10 +89,10 @@ bool testMostFrequentElementSearch(void)
     int array1[SIZE_TWO] = { 1, 2, 2, 2, 3, 4, 5, 6, 7, 8 };
     int identicalElements[SIZE_TWO] = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
     int differentElements[SIZE_TWO] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    return (mostFrequentElementSearch(array, SIZE_TWO) == 8 &&
+    return mostFrequentElementSearch(array, SIZE_TWO) == 8 &&
         mostFrequentElementSearch(array1, SIZE_TWO) == 2 &&
         mostFrequentElementSearch(identicalElements, SIZE_TWO) == 5 &&
-        mostFrequentElementSearch(differentElements, SIZE_TWO) == 1);
+        mostFrequentElementSearch(differentElements, SIZE_TWO) == 1;
 }
 
 int main()
