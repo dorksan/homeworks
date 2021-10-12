a2ca1c6970038b08bfab487acaec2d746b9849bf
dorksan <alinchasaur@yandex.ru>
2021-10-12 21:46:11 +0300

    Fix errors

--- projects/3rd week/search/main.c
+++ projects/3rd week/search/main.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TEST_ONE 25
+#define TEST_TWO 10
+
 void qSort(int* array, int size)
 {
     int left = 0;
@@ -42,10 +45,9 @@ int binarySearch(int* array, int number, int size)
 {
     int left = 0;
     int right = size - 1;
-    int middle = 0;
     while (left <= right)
     {
-        middle = (left + right) / 2;
+        int middle = (left + right) / 2;
         if (number < array[middle])
         {
             right = middle - 1;
@@ -56,32 +58,33 @@ int binarySearch(int* array, int number, int size)
         }
         else
         {
-            return array[middle];
+            return middle;
         }
     }
-    return 0;
+    return -1;
 }
 
 bool testQSort(void)
 {
-    int array[25] = { 17, 34, 121, 18, 61, 58, 235, 104, 674, 19, 38, 33, 410, 20, 9, 12, 4, 11, 6, 27, 1, 42, 8, 31, 26 };
-    int array1[25] = { 1, 4, 6, 8, 9, 11, 12, 17, 18, 19, 20, 26, 27, 31, 33, 34, 38, 42, 58, 61, 104, 121, 235, 410, 674 };
-    int sortArray[25] = { 1, 4, 6, 8, 9, 11, 12, 17, 18, 19, 20, 26, 27, 31, 33, 34, 38, 42, 58, 61, 104, 121, 235, 410, 674 };
-    qSort(array, 25);
-    qSort(array1, 25);
-    for (int i = 0; i < 25; i++)
+    int array[TEST_ONE] = { 17, 34, 121, 18, 61, 58, 235, 104, 674, 19, 38, 33, 410, 20, 9, 12, 4, 11, 6, 27, 1, 42, 8, 31, 26 };
+    int array1[TEST_ONE] = { 1, 4, 6, 8, 9, 11, 12, 17, 18, 19, 20, 26, 27, 31, 33, 34, 38, 42, 58, 61, 104, 121, 235, 410, 674 };
+    int sortArray[TEST_ONE] = { 1, 4, 6, 8, 9, 11, 12, 17, 18, 19, 20, 26, 27, 31, 33, 34, 38, 42, 58, 61, 104, 121, 235, 410, 674 };
+    qSort(array, TEST_ONE);
+    qSort(array1, TEST_ONE);
+    for (int i = 0; i < TEST_ONE; i++)
     {
         if (array[i] != sortArray[i] || array1[i] != sortArray[i])
         {
-            return 0;
+            return false;
         }
     }
+    return true;
 }
 
 bool testBinarySearch()
 {
-    int array[10] = { 1, 3, 7, 8, 12, 19, 26, 34, 42, 51 };
-    return binarySearch(array, 3, 10) == 3 && binarySearch(array, 11, 10) == 0;
+    int array[TEST_TWO] = { 1, 3, 7, 8, 12, 19, 26, 34, 42, 51 };
+    return binarySearch(array, 3, TEST_TWO) == 1 && binarySearch(array, 11, TEST_TWO) == -1;
 }
 
 int main()
@@ -91,14 +94,15 @@ int main()
         printf("Ошибка");
         return -1;
     }
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
     printf("Введите длину массива n: ");
     int n = 0;
     scanf_s("%d", &n);
     int* array = calloc(n, sizeof(int));
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", array[i] = rand() % 100);
+        array[i] = rand() % 100;
+        printf("%d ", array[i]);
     }
     qSort(array, n);
     printf("\nВведите количество случайных чисел k: ");
@@ -107,7 +111,7 @@ int main()
     for (int i = 0; i < k; i++)
     {
         const int number = rand() % 100;
-        if (binarySearch(array, number, n) == 0)
+        if (binarySearch(array, number, n) == -1)
         {
             printf("\nЧисло %d не содержится в массиве.", number);
         }
@@ -116,4 +120,5 @@ int main()
             printf("\nЧисло %d содержится в массиве.", number);
         }
     }
+    free(array);
 }
