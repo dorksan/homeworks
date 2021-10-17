338e3ff3daae58c03d541019ecd7cef826d4c1bd
dorksan <alinchasaur@yandex.ru>
2021-10-18 02:04:54 +0300

    And fix modules again

--- projects/homework 4/binary_representation/binaryAddition.c
+++ projects/homework 4/binary_representation/binaryAddition.c
@@ -2,7 +2,7 @@
 
 #define SIZE 8
 
-void binaryAddition(int* firstBinaryNumber, int* secondBinaryNumber, int* sum)
+void binaryAddition(int* firstBinaryNumber, int* secondBinaryNumber, int* result)
 {
     int temp = 0;
     for (int i = SIZE; i > 0; i--)
@@ -10,22 +10,22 @@ void binaryAddition(int* firstBinaryNumber, int* secondBinaryNumber, int* sum)
         int sum = firstBinaryNumber[i] + secondBinaryNumber[i] + temp;
         if (sum == 0)
         {
-            sum[i] = 0;
+            result[i] = 0;
             temp = 0;
         }
         if (sum == 1)
         {
-            sum[i] = 1;
+            result[i] = 1;
             temp = 0;
         }
         if (sum == 2)
         {
-            sum[i] = 0;
+            result[i] = 0;
             temp = 1;
         }
         if (sum == 3)
         {
-            sum[i] = 1;
+            result[i] = 1;
             temp = 1;
         }
     }
