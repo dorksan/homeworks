b99ca1d6e629df7893f2317b0ae5a77ce9a13257
dorksan <alinchasaur@yandex.ru>
2021-11-07 02:29:50 +0300

    Small changes

--- projects/homework 5/sort_station/sortStation.h
+++ projects/homework 5/sort_station/sortStation.h
@@ -4,6 +4,7 @@ typedef struct Stack Stack;
 
 // Перевод выражения из инфиксной в постфиксную форму
 // Коды ошибок:
+// 0 - выполнено без ошибок
 // 1 - пропущена открывающая скобка
 // 2 - пропущена закрывающая скобка
 void sortStation(char* infixString, char* postfixString, int* errorCode);
