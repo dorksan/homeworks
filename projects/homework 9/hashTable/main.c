e86abaa72cd7772235b81eb10939ae5fb753409d
dorksan <alinchasaur@yandex.ru>
2021-12-01 23:47:26 +0300

    Add hash function

--- projects/homework 9/hashTable/main.c
+++ projects/homework 9/hashTable/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MOD 1021
+
+int hashFunction(const char* string)
+{
+    const p = 67;
+    const length = strlen(string);
+    long long hash = 0;
+    for (int i = 0; i < length; i++)
+    {
+        hash = (hash * p + string[i]) % MOD;
+    }
+    return abs(hash);
+}
 
 int main()
 {
-
+    printf("%d", hashFunction("фффф"));
 }
