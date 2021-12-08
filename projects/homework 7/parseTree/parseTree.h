f1a91b9b2c176b13058c66eed4b06e5ce00ae038
dorksan <alinchasaur@yandex.ru>
2021-12-08 20:50:03 +0300

    Add comments for functions

--- projects/homework 7/parseTree/parseTree.h
+++ projects/homework 7/parseTree/parseTree.h
@@ -2,12 +2,17 @@
 
 typedef struct Node Node;
 
+// Создает узел дерева
 Node* createNode();
 
+// Создает дерево разбора
 Node* createTree(const char* string);
 
+// Подсчитывает результат выражения
 int getResult(Node* node);
 
+// Выводит дерево в строку
 void getString(Node* node, char* string, int size);
 
+// Рекурсивно удаляет дерево
 void deleteTreeRecursive(Node* node);
